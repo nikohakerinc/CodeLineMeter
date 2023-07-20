f48564de9dfd59778c11782982ccdb214ae09ced
Nikolay P <hakermkuk@gmail.com>
2023-07-20 14:17:05 +0300

    optimisation code

--- main_code.py
+++ main_code.py
@@ -18,9 +18,6 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-GIT_URL = os.getenv('GIT_URL')
-GIT_USERNAME = os.getenv('GIT_USERNAME')
-GIT_TOKEN = os.getenv('GIT_TOKEN')
 
 class CodeLineMeter:
     def __init__(self):
@@ -28,9 +25,9 @@ class CodeLineMeter:
         self.log_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'logs')
         self.reports_dir = 'reports'
         self.repo_folder = os.path.join(os.path.dirname(os.path.abspath(__file__)), "repo")
-        self.projects = [] # Список проектов
-        self.total = 0 # Общий счётчик строк
-        self.result = {} # Словарь с результатами
+        self.projects = []  # Список проектов
+        self.total = 0  # Общий счётчик строк
+        self.result = {}  # Словарь с результатами
         self.start_time = None
         self.global_start_time = datetime.datetime.now()
         self.conn = None
@@ -40,52 +37,39 @@ class CodeLineMeter:
         self.create_table()
         self.read_projects()
 
-    # Импорт JSON файла с языками программирования с соответствующими форматами файлов
     def load_languages(self):
         with open('lang_dict.json', 'r') as json_file:
-            json_data = json_file.read()
-        return json.loads(json_data)
+            return json.load(json_file)
 
-    # Создание рабочих папок
     def create_directories(self):
-        if not os.path.exists(self.log_dir):
-            os.makedirs(self.log_dir)
-        if not os.path.exists(self.reports_dir):
-            os.makedirs(self.reports_dir)
-        if not os.path.exists(self.repo_folder):
-            os.makedirs(self.repo_folder)
+        os.makedirs(self.log_dir, exist_ok=True)
+        os.makedirs(self.reports_dir, exist_ok=True)
+        os.makedirs(self.repo_folder, exist_ok=True)
 
-    # Настройка логирования
     def setup_logging(self):
         logging.basicConfig(filename=os.path.join(self.log_dir, 'info.log'), level=logging.INFO,
                             format='%(levelname)s: %(asctime)s %(message)s', datefmt='%d/%m/%Y %H:%M:%S')
 
-    # Создаём базу для альтернативного отчёта
     def create_table(self):
         if not self.conn:
             self.conn = sqlite3.connect(os.path.join(self.reports_dir, 'code_stats.db'))
             self.c = self.conn.cursor()
 
-            # Базовый SQL запрос
             create_table_sql = '''
                 CREATE TABLE IF NOT EXISTS projects (
                     project_url TEXT,
                     project_name TEXT,
             '''
-            # Добавление колонок для языков динамически из значений JSON
             for lang, extensions in self.languages.items():
                 lang_col_name = lang.lower().replace(' ', '')
                 create_table_sql += f'"{lang_col_name}" INTEGER, '
-            # Завершение SQL-запроса, добавление в таблицу колонки total lines
             create_table_sql += 'total_lines INTEGER)'
             self.c.execute(create_table_sql)
 
-    # Считывание списка проектов из файла 'project.txt'
     def read_projects(self):
         with open('project.txt', 'r') as f:
-            self.projects = [project.strip() for project in f.readlines() if project.strip()]
+            self.projects = [project.strip() for project in f if project.strip()]
 
-    # Клонирование репозиториев
     def clone_repository(self, project, i, line_count):
         result = {}
         repo_url = project.strip().replace("https://", "")
@@ -94,7 +78,7 @@ class CodeLineMeter:
 
         start_time = datetime.datetime.now()
         logging.info(f"Start cloning a repository ({i}/{line_count}): {project_dir}")
-        os.system(f"git clone https://{GIT_USERNAME}:{GIT_TOKEN}@{repo_url} {repo_dir}")
+        os.system(f"git clone https://{os.getenv('GIT_USERNAME')}:{os.getenv('GIT_TOKEN')}@{repo_url} {repo_dir}")
         logging.info(f"Finish cloning a repository: {project_dir}")
         timer_rounded = round(((datetime.datetime.now() - start_time).total_seconds()), 2)
         logging.info(f"Completed in: {timer_rounded} seconds")
@@ -115,18 +99,18 @@ class CodeLineMeter:
                                 total_lines += len(non_empty_lines)
                         except UnicodeDecodeError:
                             logging.warning(f"Failed to read file: {file_path}")
-        
-        result[repo_url] = (project_dir,) + tuple(language_lines.values()) + (total_lines,)                  
+
+        result[repo_url] = (project_dir,) + tuple(language_lines.values()) + (total_lines,)
         self.write_to_database(repo_url, project_dir, language_lines, total_lines)
 
-        # Удаление содержимого папки repo_dir, оставляя папку 'repo'
         shutil.rmtree(repo_dir, ignore_errors=True)
-        os.system(f"rm -rf {repo_dir} 2> /dev/null")    # Для Linux/Mac
-        os.system(f"rd /s /q {repo_dir} 2> nul")        # Для Windows
+        if os.name == 'nt':
+            os.system(f"rd /s /q {repo_dir} 2> nul")
+        else:
+            os.system(f"rm -rf {repo_dir} 2> /dev/null")
                 
         return result, total_lines
 
-    # Запись подсчётов по каждому проекту в таблицу 'projects' в БД 'code_state.db'
     def write_to_database(self, repo_url, project_dir, language_lines, total_lines):
         if not self.conn:
             self.conn = sqlite3.connect(os.path.join(self.reports_dir, 'code_stats.db'))
@@ -140,7 +124,6 @@ class CodeLineMeter:
         self.c.execute(insert_values, values)
         self.conn.commit()
 
-    # Управление последовательностью выполнения
     def analyze_projects(self):
         line_count = len(self.projects)
         logging.info(f"Analyzing {line_count} projects...")
@@ -157,52 +140,42 @@ class CodeLineMeter:
                 logging.error(f"Error analyzing project: {self.repo_folder}.git\nError message: {str(e)}")
 
         logging.info(f"Analysis completed in {(str((datetime.datetime.now() - self.start_time)).split('.')[0])}")
-        shutil.rmtree(self.repo_folder)
+        shutil.rmtree(self.repo_folder, ignore_errors=True)
         
         return self.result, self.total
 
     def generate_visualizations(self, result, languages, reports_dir):
-        # Инициализация словаря 'temp' на основе словаря 'languages'
         temp = {key: 0 for key in languages}
-        # Чтение словаря 'result' и суммирование значений по колонкам (языкам) и дальнейшего построения диаграмм
         for values in result.values():
             language_lines = values[1:-1]
             for language, lines in zip(temp.keys(), language_lines):
                 temp[language] += lines
 
-        # Фильтрация колонок содержащих только ненулевых значений
         temp_filtered = {k: v for k, v in temp.items() if v != 0}
-        # Создание DataFrame и сортировка от большего суммарного значения к меньшему
         df = pd.DataFrame({'Language': list(temp_filtered.keys()), 'Count': list(temp_filtered.values())})
         df = df.sort_values('Count', ascending=False)
 
-
-        # Построение гистограммы
         with plt.style.context('cyberpunk'):
             ax = df.plot(x='Language', kind='bar', stacked=False, alpha=0.8, figsize=(16,9), legend=False)
             ax.set_ylim(top=ax.get_ylim()[1] * 1.1)
-            # Вывод значений над каждым столбцом Language
             for p in ax.patches:
                 ax.annotate(str(p.get_height()), (p.get_x() + p.get_width() / 2, p.get_height()),
-                            ha='center', va='bottom', rotation = 30)
+                            ha='center', va='bottom', rotation=30)
             plt.xticks(range(len(df)), df['Language'], fontsize=10, ha='center')
             plt.gcf().autofmt_xdate()
             plt.tight_layout()
         histogram_chart_pdf_path = os.path.join(reports_dir, 'histogram_chart.pdf')
         plt.savefig(histogram_chart_pdf_path, format="pdf", dpi=300, orientation='portrait', bbox_inches='tight')
 
-
-        # Построение кольцевой диаграммы
         with plt.style.context('cyberpunk'):
             labels = [f"{lang} ({count})" for lang, count in zip(df['Language'], df['Count'])]
             sizes = df['Count']
             explode = (0.1,) * len(df)
             colors = plt.cm.plasma(np.linspace(0.2, 1, len(df['Language'])))
-            # Создание круговой диаграммы
             fig, ax = plt.subplots(figsize=(16, 9))
             wedges, labels, text_handles = ax.pie(sizes, explode=explode, labels=labels, colors=colors,
-                    autopct=lambda pct: f"{pct:.1f}%" if pct > 2.0 else None, shadow=True,
-                    startangle=45, wedgeprops=dict(width=0.5), rotatelabels=True, pctdistance=0.75)
+                                                  autopct=lambda pct: f"{pct:.1f}%" if pct > 2.0 else None, shadow=True,
+                                                  startangle=45, wedgeprops=dict(width=0.5), rotatelabels=True, pctdistance=0.75)
             for label, text_handle in zip(labels, text_handles):
                 if label.get_text() == '':
                     text_handle.set_alpha(0)
@@ -210,8 +183,6 @@ class CodeLineMeter:
         donat_chart_pdf_path = os.path.join(reports_dir, 'donat_chart.pdf')
         plt.savefig(donat_chart_pdf_path, format="pdf", dpi=300, orientation='portrait', bbox_inches='tight')
 
-
-    # Запись данных из словаря result в файл count.csv
     def write_results_to_file(self, result, languages, total, reports_dir):
         count_csv_path = os.path.join(reports_dir, 'count.csv')
         with open(count_csv_path, 'a') as f:
@@ -222,8 +193,7 @@ class CodeLineMeter:
                 f.write(line)
             f.write('\n\n')
             f.write(f"Total lines of code:; {total}")
-         
-    # Экспорт данных из БД в countdb.csv
+
     def write_dbdata_to_file(self, languages, total, reports_dir):
         if not self.conn:
             self.conn = sqlite3.connect(os.path.join(self.reports_dir, 'code_stats.db'))
@@ -233,72 +203,21 @@ class CodeLineMeter:
             keys = ";".join(languages.keys())
             f.write(f"Project URL;Project Name;{keys};Total lines of code\n")
             for row in self.c.execute("SELECT * FROM projects"):
-                project_data = [str(item) for item in row[:-1]]  # Конвертация int to string
+                project_data = [str(item) for item in row[:-1]]
                 project_lines = str(row[-1])
                 f.write(f"{';'.join(project_data)};{project_lines}\n")
             f.write(f"\n\nTotal lines of code:; {total}")
         self.conn.close()
 
-    @staticmethod
-    def pack_and_send_reports():
-        # Создание ZIP архива
-        shutil.make_archive("reports", 'zip', ".", "reports")
-        # Параметры отправки почты
-        from_email = os.getenv('FROM_EMAIL')
-        to_email = os.getenv('TO_EMAIL')
-        password = os.getenv('EMAIL_PASSWORD')
-        smtp_server = os.getenv('SMTP_SERVER')
-        smtp_port = int(os.getenv('SMTP_PORT'))
 
-        # Создание объекта MIMEMultipart
-        message = MIMEMultipart()
-        message['From'] = from_email
-        message['To'] = to_email
-        message['Subject'] = 'Code Line Meter Reports'
-
-        # Вложение ZIP архива
-        attachment = MIMEBase('application', 'zip')
-        with open('reports.zip', 'rb') as file:
-            attachment.set_payload(file.read())
-        encoders.encode_base64(attachment)
-        attachment.add_header('Content-Disposition', 'attachment', filename='reports.zip')
-        message.attach(attachment)
-        message_text = '''Сформирован отчет по количеству строк кода.\nВложенный ZIP архив содержит подробные данные.'''
-        message.attach(MIMEText(message_text, 'plain'))
-
-        # Отправка письма
-        try:
-            with smtplib.SMTP_SSL(smtp_server, smtp_port) as server:
-                # server.starttls()
-                server.login(from_email, password)
-                server.send_message(message)
-            logging.info(f"Reports sent to {to_email} is successfull!")
-            print("Reports sent successfully!")
-        except Exception as e:
-            print(f"Error sending reports: {str(e)}")
-            logging.info(f"Error sending reports: {str(e)}")
-            
-    def remove_reports(self, reports_dir):
-        shutil.rmtree(reports_dir, ignore_errors=True)
-        os.system(f"rm -rf {reports_dir} 2> /dev/null")    # Для Linux/Mac
-        os.system(f"rd /s /q {reports_dir} 2> nul")        # Для Windows
-        zip_file = os.path.join(os.getcwd(), "reports.zip")
-        if os.path.exists(zip_file):
-            os.remove(zip_file)
-        logging.info("Reports dir and reports.zip removed successfully!")
-        print("Reports removed successfully!")
-
-
-def main():
-    code_stats = CodeLineMeter()
-    result, total_lines = code_stats.analyze_projects()
-    code_stats.generate_visualizations(result, code_stats.languages, code_stats.reports_dir)
-    code_stats.write_results_to_file(result, code_stats.languages, total_lines, code_stats.reports_dir)
-    code_stats.write_dbdata_to_file(code_stats.languages, total_lines, code_stats.reports_dir)
-    CodeLineMeter.pack_and_send_reports()
-    code_stats.remove_reports(code_stats.reports_dir)
-    print(f"Total lines of code: {total_lines}")
+    def run(self):
+        result, total_lines = self.analyze_projects()
+        self.generate_visualizations(result, self.languages, self.reports_dir)
+        self.write_results_to_file(result, self.languages, total_lines, self.reports_dir)
+        self.write_dbdata_to_file(self.languages, total_lines, self.reports_dir)
+        print(f"Total lines of code: {total_lines}")
 
 
 if __name__ == '__main__':
-    main()
+    code_stats = CodeLineMeter()
+    code_stats.run()
